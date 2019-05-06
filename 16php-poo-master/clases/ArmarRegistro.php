6c58e4beb8b3ae7708e30aa0101208ef3f173f3c
Mauro <45835713+mauromaiani@users.noreply.github.com>
2019-05-06 17:36:43 -0300

    Add files via upload

---
+++ 16php-poo-master/clases/ArmarRegistro.php
@@ -0,0 +1,13 @@
+<?php
+class ArmarRegistro{
+    public function armarUsuario($registro){
+        $usuario = [
+            "nombre"=>$registro->getNombre(),
+            "email"=>$registro->getEmail(),
+            "password"=> Encriptar::hashPassword($registro->getPassword()),
+            "perfil"=>1
+        ];
+
+        return $usuario;
+    }
+}
