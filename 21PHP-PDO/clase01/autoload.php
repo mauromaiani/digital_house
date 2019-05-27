17c3dbaf2e4c0725713516bbc6c697cbfc5475d2
Mauro <45835713+mauromaiani@users.noreply.github.com>
2019-05-27 17:36:22 -0300

    php pdo clase 01

---
+++ 21PHP-PDO/clase01/autoload.php
@@ -0,0 +1,37 @@
+<?php
+require_once("clases/BaseMYSQL.php");
+//Declaro mis variables
+$host = "localhost";
+$bd = "movies_db";
+$usuario = "root";
+$password = "root";
+$puerto = "3306";
+//Ojo: Para los que trabajan con MAC: deben colocar el puerto: 8889
+
+$pdo = BaseMYSQL::conexion($host,$bd,$usuario,$password,$puerto);
+
+?>
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Practicando PHP-PDO</title>
+  </head>
+  <body>
+    <h1></h1>
+    <a class= "" href="peliculas.php">Pelicula</a>
+    <a class= "" href="series.php">Series</a>
+    <a class= "" href="generos.php">Generos</a>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
